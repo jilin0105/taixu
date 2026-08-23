f1d1754e8b123e2cff4a80d8a72085271c7cb4be
wkbin <wangkebin1997@gmail.com>
2026-08-24 02:30:29 +0800

    Release v0.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,8 +5,8 @@ import java.util.Properties
 import org.gradle.kotlin.dsl.configure
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
-val appVersionName = "0.3.0"
-val appVersionCode = 3
+val appVersionName = "0.4.0"
+val appVersionCode = 4
 
 plugins {
     alias(libs.plugins.android.application)
