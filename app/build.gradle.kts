0547d4ae87f7dad615d605896a3d5901b305c57d
王克斌 <wangkebin1997@gmail.com>
2026-08-24 13:27:59 +0800

    Release v0.4.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,8 +5,8 @@ import java.util.Properties
 import org.gradle.kotlin.dsl.configure
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
-val appVersionName = "0.4.0"
-val appVersionCode = 4
+val appVersionName = "0.4.1"
+val appVersionCode = 5
 
 plugins {
     alias(libs.plugins.android.application)
